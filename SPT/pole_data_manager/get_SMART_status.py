7e03db0e14d35556123a4286d98503e7f537e010
Benedikt Riedel <benedikt.riedel@gmail.com>
2016-09-04 20:48:17 -0500

    Adding script to query SMART status

---
+++ SPT/pole_data_manager/get_SMART_status.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import glob
+import subprocess
+import re
+
+with open(inputfilename, "rt") as f:
+    serial_nums = []
+    for l in f:
+        l = l.rstrip("\n")
+        l.split(" ")
+    # Looping through the disks in /dev/
+    for d in glob.glob("/dev/sd*"):
+        # Calls the smartctl
+        c = subprocess.Popen("smartctl -a {0}".format(d),
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE,
+                             shell=True)
+        output, error = c.communicate()
+        re_serial_num = re.findall("Serial number:(.*)",
+                                   output, re.MULTILINE)
+        re_smart_status = re.findall("SMART Health Status:(.*)",
+                                     output, re.MULTILINE)
+        if re_serial_num[0].lstrip(":").replace(" ", "") in serial_nums:
+            smart_status = re_smart_status.replace(" ", "")
+            if smart_status != "OK":
+                #Tell winterovers
