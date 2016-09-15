72c8f41b8fbbf46a5b670f5e09f5005ebeb33dd3
Benedikt Riedel <benedikt.riedel@gmail.com>
2016-09-14 19:09:53 -0500

    Debugging bzip2 build

--- SPT/cvmfs/builders/tools/bzip2.py
+++ SPT/cvmfs/builders/tools/bzip2.py
@@ -25,10 +25,13 @@ def install(dir_name, version=None):
                 raise Exception('bzip2 failed to make')
             if subprocess.call(['make','clean'], cwd = bzip2_dir):
                 raise Exception('bzip2 failed to install')
-            if subprocess.call(['make', '-f Makefile-libbz2_so','PREFIX=%s' % dir_name], cwd = bzip2_dir):
+            if subprocess.call(['make', '-f', 'Makefile-libbz2_so','PREFIX=%s' % dir_name], cwd = bzip2_dir):
                 raise Exception('bzip2 failed to install')
-            shutil.copy2("libbz2.so.1.0.6", os.path.join(dir_name, "lib"))
-            shutil.copy2("libbz2.so.1.0", os.path.join(dir_name, "lib"))
+
+            shutil.copy2(os.path.join(bzip2_dir, "libbz2.so.1.0.6"), 
+                         os.path.join(dir_name, "lib"))
+            shutil.copy2(os.path.join(bzip2_dir, "libbz2.so.1.0"), 
+                         os.path.join(dir_name, "lib"))
             os.symlink(os.path.join(dir_name, "lib", "libbz2.so.1.0"), os.path.join(dir_name, "lib", "libbz2.so"))
         finally:
             shutil.rmtree(tmp_dir)
