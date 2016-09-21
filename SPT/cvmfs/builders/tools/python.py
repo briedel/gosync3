2527aff9cf444e1f78bf029dbaf7674cfa442fbc
Benedikt Riedel <benedikt.riedel@gmail.com>
2016-09-21 16:52:19 -0500

    Using distutils rather than string operations

--- SPT/cvmfs/builders/tools/python.py
+++ SPT/cvmfs/builders/tools/python.py
@@ -5,6 +5,7 @@ import subprocess
 import tempfile
 import shutil
 
+from distutils.version import LooseVersion
 from build_util import wget, unpack, version_dict
 
 def install(dir_name,version=None):
@@ -19,21 +20,33 @@ def install(dir_name,version=None):
             unpack(path,tmp_dir)
             python_dir = os.path.join(tmp_dir,'Python-'+version)
             if subprocess.call([os.path.join(python_dir,'configure'),
-                                '--prefix',dir_name,'--enable-shared']
-                               ,cwd=python_dir):
+                                '--prefix',dir_name,'--enable-shared'],
+                               cwd=python_dir):
                 raise Exception('python failed to configure')
             if subprocess.call(['make'],cwd=python_dir):
                 raise Exception('python failed to make')
             if subprocess.call(['make','install'],cwd=python_dir):
                 raise Exception('python failed to install')
             
+            v = LooseVersion(version)
+            # Python 3 specific symlinks
+            if v.version[0] == 3:
+                version_short = v.version[:2] 
+                os.symlink(os.path.join(dir_name,'bin','python3'),
+                           os.path.join(dir_name,'bin','python'))
+                os.symlink(os.path.join(dir_name, 'bin', 'python3-config'),
+                           os.path.join(dir_name, 'bin', 'python-config'))
+                os.symlink(os.path.join(dir_name, 'lib', 'pkgconfig', 'python3.pc'),
+                           os.path.join(dir_name, 'lib', 'pkgconfig', 'python.pc'))
+                os.symlink(os.path.join(dir_name, 'include', 'python%sm' % version_short),
+                           os.path.join(dir_name, 'include', 'python%s' % version_short)) 
             # check for modules
             for m in ('sqlite3','zlib','bz2','_ssl','_curses','readline'):
                 if subprocess.call([os.path.join(dir_name,'bin','python'),
-                                    '-c','import '+m]):
+                                    '-c','"import %s"' % m]):
                     if os.path.exists(os.path.join(dir_name,'bin','python')):
                         os.remove(os.path.join(dir_name,'bin','python'))
-                    raise Exception('failed to build with '+m+' support')
+                        raise Exception('failed to build with '+m+' support')
         finally:
             shutil.rmtree(tmp_dir)
 
