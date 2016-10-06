192760fa68578acf663b693f58a4cb9ee0f5f62d
Benedikt Riedel <benedikt.riedel@gmail.com>
2016-10-06 18:53:07 -0500

    Adding new north data manager

---
+++ SPT/north_data_manager/spt_data_management_schema.sql
@@ -0,0 +1,13 @@
+-- Schema for SPT data management.
+
+-- files are the information about the individual files
+-- filename, checksum (SHA512), file size, disk on Primary storage,
+-- disk on Copy storage
+create table files (
+    filenumber   INTEGER primary key autoincrement not null,
+    filename     TEXT not null,
+    checksum     TEXT not null,
+    filesize     INTEGER not null,
+    backup_nersc NUMERIC,
+    backup_anl   NUMERIC
+);
