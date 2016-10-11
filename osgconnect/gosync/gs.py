27dd607730b6728d92902abf14c951469a43941e
Benedikt Riedel <benedikt.riedel@gmail.com>
2016-10-11 18:30:01 -0500

    Accessing nexus server for first time

--- osgconnect/gosync/gs.py
+++ osgconnect/gosync/gs.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python
-from future import print_function
-import os
-import sys
-import ConfigParser
+from __future__ import print_function
+# import os
+# import sys
 import logging
-import getopt
-import time
-import glob
-import json
-import re
-import fnmatch
-import errno
-import random
-import socket
-import grp
-import pwd
+# import getopt
+# import time
+# import glob
+# import json
+# import re
+# import fnmatch
+# import errno
+# import random
+# import socket
+# import grp
+# import pwd
 
 from optparse import OptionParser
 
@@ -32,44 +31,45 @@ except:
                        "or Globus Nexus. Exiting"))
         raise RuntimeError()
 
-parser = OptionParser()
-parser.add_option("--config", dest="config",
-                  help="config file to use",)
-parser.add_option("-v", "--verbosity", dest="verbosity",
-                  help="Set logging level", default=3)
-parser.add_option("--format", dest="format",
-                  help="Output format to use")
-parser.add_option("--force", dest="force", action="store_true",
-                  default=False, help="Force update information")
-parser.add_option("--baseurl", dest="baseurl", default=None,
-                  help="Base URL to use")
-parser.add_option("--portal", dest="portal", default=None,
-                  help="Portal to use")
-parser.add_option("--parent", dest="parent", default=None,
-                  help="Parent group to use")
-parser.add_option("--top", dest="top", default=None,
-                  help="Top group to use")
-parser.add_option("--group", dest="group", default=None,
-                  help="Group to use")
-parser.add_option("--user", dest="user", default=None,
-                  help="User to use")
 
-(options, args) = parser.parse_args()
-
-level = {
-    1: logging.ERROR,
-    2: logging.WARNING,
-    3: logging.INFO,
-    4: logging.DEBUG
-}.get(options.verbosity, logging.DEBUG)
-
-def main():
+def main(options, args):
     config = parse_config(options.config)
     nexusconfig = {"server": config['globus']['server'],
-                   "client": user,
-                   "client_secret": self.get(section, 'secret')}
+                   "client": "admin",
+                   "client_secret": config['secrets']['connect']}
     client = GlobusOnlineRestClient(config=nexusconfig)
+    print(client)
     print(client.get_group_list(my_roles=['admin']))
 
+
 if __name__ == '__main__':
-    main()
+
+    parser = OptionParser()
+    parser.add_option("--config", dest="config", default="gosync.conf",
+                      help="config file to use",)
+    parser.add_option("-v", "--verbosity", dest="verbosity",
+                      help="Set logging level", default=3)
+    parser.add_option("--format", dest="format",
+                      help="Output format to use")
+    parser.add_option("--force", dest="force", action="store_true",
+                      default=False, help="Force update information")
+    parser.add_option("--baseurl", dest="baseurl", default=None,
+                      help="Base URL to use")
+    parser.add_option("--portal", dest="portal", default=None,
+                      help="Portal to use")
+    parser.add_option("--parent", dest="parent", default=None,
+                      help="Parent group to use")
+    parser.add_option("--top", dest="top", default=None,
+                      help="Top group to use")
+    parser.add_option("--group", dest="group", default=None,
+                      help="Group to use")
+    parser.add_option("--user", dest="user", default=None,
+                      help="User to use")
+    (options, args) = parser.parse_args()
+    level = {
+        1: logging.ERROR,
+        2: logging.WARNING,
+        3: logging.INFO,
+        4: logging.DEBUG
+    }.get(options.verbosity, logging.DEBUG)
+    main(options, args)
