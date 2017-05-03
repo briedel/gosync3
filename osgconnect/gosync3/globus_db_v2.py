0a8f44ca81f9600e25ae7b0420e4e753dc988432
Benedikt Riedel <benedikt.riedel@gmail.com>
2017-05-03 17:29:09 -0500

    First attempt at automating updating the json from globus directly

--- osgconnect/gosync3/globus_db_v2.py
+++ osgconnect/gosync3/globus_db_v2.py
@@ -2,6 +2,8 @@ from __future__ import print_function
 import logging as log
 import globus_sdk
 import six
+from itertools import groupby
+from operator import itemgetter
 from globus_nexus_client import NexusClient, LegacyGOAuthAuthorizer
 
 
@@ -34,9 +36,13 @@ class globus_db(object):
         self.legacy_client = self.get_legacy_client(
             self.config["globus"]["root_user"]["username"],
             self.config["globus"]["root_user"]["secret"])
+        self.all_groups = None
+
+    """
+    Gloubus Client methods
+    """
 
     def get_tokens(self, username):
-        print(username)
         if username == self.config["globus"]["root_user"]["username"]:
             auth_token = self.config[
                 "globus"]["root_user"]["auth_refresh_token"]
@@ -70,7 +76,6 @@ class globus_db(object):
         Returns:
             client: Globus Nexus RESTful client
         """
-        print(username)
         if username is None:
             username = self.config["globus"]["root_user"]["username"]
 
@@ -92,21 +97,35 @@ class globus_db(object):
         log.debug("Got Globus Nexus client")
         return auth_client, nexus_client
 
-    def get_group(self, group_name):
-        groups = self.get_groups()
-        for group in groups:
-            if group["name"] == group_name:
-                return group
 
-    def get_groups(self):
-        auth_client, nexus_client = self.get_globus_client()
-        roles = self.get_roles()
-        groups = nexus_client.list_groups(
-            fields="id,name",
-            for_all_identities=True,
-            include_identity_set_properties=True,
-            my_roles=roles)
+    """
+    Group Methods
+    """
+
+    def get_group(self, group_name):
+        group = self.get_groups(group_name)
+        return group[0]
+
+    def get_groups(self, group_names=None):
+        if self.all_groups is None:
+            all_groups = self.get_all_groups()
+        if isinstance(group_names, six.string_types):
+            group_names = [group_names]
+        elif group_names is None:
+            return all_groups
+        groups = [group for group in all_groups
+                  if group["name"] in group_names]
         return groups
+
+    def get_all_groups(self, update=True):
+        if self.all_groups is not None and update:
+            return self.all_groups
+        all_groups = self.get_user_groups(
+            [self.config["globus"]["root_user"]["username"]],
+            using_auth=True)
+        self.all_groups = all_groups[self.config[
+            "globus"]["root_user"]["username"]]
+        return self.all_groups
 
     def get_user_info_for_db(self, username):
         raise NotImplementedError()
@@ -122,9 +141,34 @@ class globus_db(object):
                  else self.config["globus"]["user"]["roles"])
         return roles
 
-    def get_user_groups(self, usernames):
+    def get_group_tree(self, username=None, root_group_uuid=None, depth=3):
+        auth_client, nexus_client = self.get_globus_client(username=username)
+        if root_group_uuid is None:
+            root_group_uuid = self.config["globus"]["groups"][
+                "root_group_uuid"]
+        roles = self.get_roles(username)
+        tree = nexus_client.get_group_tree(root_group_uuid,
+                                           depth=depth,
+                                           my_roles=roles)
+        return tree
+
+    """
+    Group Membership Methods
+    """
+
+    def get_user_groups(self, usernames=None, using_auth=False):
         if isinstance(usernames, six.string_types):
             usernames = [usernames]
+        #### TODO: NEED TO FIX THIS FOR NEXUS
+        elif usernames is None:
+            usernames = [self.config["globus"]["root_user"]["username"]]
+        if using_auth:
+            membership = self.get_user_groups_auth(usernames)
+        else:
+            membership = self.get_user_groups_nexus(usernames)
+        return membership
+
+    def get_user_groups_auth(self, usernames):
         membership = {}
         for user in usernames:
             auth_client, nexus_client = self.get_globus_client(
@@ -137,26 +181,30 @@ class globus_db(object):
                 my_roles=roles)
         return membership
 
-    def get_group_tree(self, username=None, root_group_uuid=None, depth=3):
-        auth_client, nexus_client = self.get_globus_client(username=username)
-        print(nexus_client)
-        print(username)
-        # print(nexus_client.get_user("connect"))
-        if root_group_uuid is None:
-            root_group_uuid = self.config["globus"]["groups"][
-                "root_group_uuid"]
-        roles = self.get_roles(username)
-        tree = nexus_client.get_group_tree(root_group_uuid,
-                                           depth=depth,
-                                           my_roles=roles)
-        return tree
+    def get_user_groups_nexus(self, usernames):
+        group_members = {}
+        for group in self.get_all_groups():
+            group_members[group["name"]] = self.get_group_members(
+                group["id"],
+                only_usernames=True)
+        member_groups = self._invert_dict_list_values(group_members)
+        if usernames != [self.config["globus"]["root_user"]["username"]]:
+            member_groups = {member: groups
+                             for member, groups in member_groups.iteritems()
+                             if member in usernames}
+        return member_groups
 
     def get_group_members(self, group_id,
-                          username=None, get_user_summary=False):
+                          username=None, get_user_summary=False,
+                          only_usernames=False):
         auth_client, nexus_client = self.get_globus_client(username=username)
         group_members = nexus_client.get_group_memberships(group_id).data
         if get_user_summary:
             group_members = [self.summarize_user(member)
+                             for member in group_members["members"]
+                             if member["status"] == "active"]
+        elif only_usernames:
+            group_members = [member["username"]
                              for member in group_members["members"]
                              if member["status"] == "active"]
         else:
@@ -196,6 +244,19 @@ class globus_db(object):
                     new_member_profile[sk] = sv
         return new_member_profile
 
+    def get_all_users(self, get_user_groups=False):
+        members = self.get_group_members(
+            self.config["globus"]["groups"]["root_group_uuid"],
+            get_user_summary=True)
+        if get_user_groups:
+            ### Need to improve this.... looping over the members twice
+            ### one time should be sufficient.... most a restriction of
+            ### using nexes
+            user_groups = self.get_user_groups()
+            for member in members:
+                member["groups"] = user_groups[member["username"]]
+        return members
+
     def check_new_members(self, group_name, group_count):
         return (self.check_group_membership_changes(group_name, group_count))
 
@@ -205,3 +266,10 @@ class globus_db(object):
     def check_group_membership_changes(self, group_name, globus_member_count):
         connect_member_count = self.connect_db.get_member_count(group_name)
         return (connect_member_count == globus_member_count)
+
+    def _invert_dict_list_values(self, dic):
+        return {x: list(t[1] for t in group)
+                for (x, group) in groupby(sorted(((j, k) for k, v in dic.items()
+                                                  for j in v), key=itemgetter(0)),
+                                          key=itemgetter(0))
+                }
--- osgconnect/gosync3/testing_globus_db_v2.py
+++ osgconnect/gosync3/testing_globus_db_v2.py
@@ -3,31 +3,116 @@ from __future__ import print_function
 import argparse
 import json
 from globus_db_v2 import globus_db
+import collections
+import unicodedata
 
 from util import parse_json_config
+
+
+def append_unix_group(globus_groups, connect_groups):
+    grps = [(group, gid["gid"]) for group, gid in connect_groups.iteritems()]
+    grps_nms = zip(*grps)[0]
+    gids = zip(*grps)[1]
+    new_groups = [ggroup["name"] for ggroup in globus_groups
+                  if ggroup["name"] not in grps_nms]
+    max_gid = max(gids)
+    for ngroup in new_groups:
+        gid = max_gid + 1 if max_gid > 100000 else 100000
+        grps.append((ngroup, gid))
+    new_grps = {group[0]: {"gid": group[1]} for group in grps}
+    return new_grps
+
+
+def find_alter_duplicate_gids(connect_groups):
+    grps = [(group, gid["gid"]) for group, gid in connect_groups.iteritems()]
+    grps_nms = zip(*grps)[0]
+    gids = list(zip(*grps)[1])
+    duplicate_gids = [item for item, count in collections.Counter(gids).items()
+                      if count > 1]
+    for dgid in duplicate_gids:
+        indices = [i for i, x in enumerate(gids) if x == dgid]
+        for iidx, idx in enumerate(indices):
+            if iidx == 0:
+                continue
+            tgid = gids[idx] + 1
+            while tgid in gids:
+                tgid += 1
+            gids[idx] = tgid
+    alter_grps = {grp: {"gid": gids[idx]}
+                  for idx, grp in enumerate(grps_nms, start=0)}
+    return alter_grps
+
+
+def decompose_sshkey(user):
+    puppet_ssh_key = collections.defaultdict(dict)
+    for key in user["ssh_pubkeys"]:
+        key_pieces = key.split(" ")
+        # Question about keys that dont have emails or hostnames
+        # What to do????
+        if len(key_pieces) > 3:
+            key_pieces = key_pieces[0:2]
+        if key_pieces[-1] != "":
+            key_pieces[-1] = key_pieces[-1].splitlines()[0]
+        else:
+            key_pieces = key_pieces[:-1]
+        if len(key_pieces) == 3:
+            puppet_ssh_key[key_pieces[-1]] = {"type": key_pieces[0],
+                                              "key": key_pieces[1]}
+        else:
+            puppet_ssh_key[user["email"]] = {
+                "type": key_pieces[0],
+                "key": key_pieces[1]}
+    return puppet_ssh_key
+
+
+def manage_user(globus_members, connect_users):
+    new_members = [gmem for gmem in globus_members
+                   if gmem["username"] not in connect_users.keys()]
+    for gmem in globus_members:
+        gmem["name"] = unicodedata.normalize(
+            "NFKD", gmem["name"]).encode('ascii', 'ignore')
+    uids = [userinfo["uid"]
+            for username, userinfo in connect_users.iteritems()]
+    max_uid = max(uids)
+    for new_member in new_members:
+        connect_users[new_members["username"]] = {
+            "auth_refresh_token": None,
+            "comment": new_member["name"],
+            "email": new_member["email"],
+            "gid": 1000,
+            "manage_group": False,
+            "nexus_refresh_token": None,
+            "shell": "/bin/bash",
+            "ssh_keys": decompose_sshkey(new_member),
+            "uid": max_uid + 1 if max_uid > 10000 else 10000,
+            "groups": new_members["groups"]
+        }
+    for gmem in globus_members:
+        if gmem["username"] in new_members.keys():
+            continue
+        json_ssh_key = decompose_sshkey(gmem)
+        connect_users[gmem["username"]]["ssh_keys"] = json_ssh_key
+        connect_users[gmem["username"]]["email"] = gmem["email"]
+        connect_users[gmem["username"]]["groups"] = gmem["groups"]
+    return connect_users
 
 
 def main(args):
     config = parse_json_config(args.config)
     globusdb = globus_db(config=config)
-    print(globusdb.get_user_groups("connect"))
-    print(globusdb.get_user_groups("briedel"))
-    print(globusdb.get_group_tree())
-    print(globusdb.get_user_info("nwhitehorn"))
-    print(globusdb.get_groups())
-    globus_groups = globusdb.get_groups()
-    group_name = [g["name"] for g in globus_groups.data]
-    print(group_name)
-    # with open("globus_groups.json", "w") as gpf:
-    #     json.dump(group_name, gpf)
-    members = globusdb.get_group_members(
-        config["globus"]["groups"]["root_group_uuid"], get_user_summary=True)
-
-    # print(members[0])
-    usernames = {member["username"]: member for member in members}
-    with open("globus_members.json", "w") as mf:
-        json.dump(usernames, mf)
-    # print()
+    globus_groups = globusdb.get_all_groups()
+    globus_members = globusdb.get_all_users(get_user_groups=True)
+    # member_groups = globusdb.get_user_groups()
+    with open("test_groups.json", "r") as infile:
+        user_groups_db = json.load(infile)
+    user_groups_db["accounts::groups"] = find_alter_duplicate_gids(
+        user_groups_db["accounts::groups"])
+    user_groups_db["accounts::groups"] = append_unix_group(
+        globus_groups, user_groups_db["accounts::groups"])
+    user_groups_db["accounts::users"] = manage_user(
+        globus_members, user_groups_db["accounts::users"])
+    with open("test_groups_v2.json", "w") as outfile:
+        json.dump(user_groups_db, outfile)
 
 
 if __name__ == '__main__':
