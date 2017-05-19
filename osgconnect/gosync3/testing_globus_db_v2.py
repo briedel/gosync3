e334dbc20bd77019a94f5ea092e33f33a3baedaf
Benedikt Riedel <benedikt.riedel@gmail.com>
2017-05-19 16:56:34 -0500

    checkpoint testing script

--- osgconnect/gosync3/testing_globus_db_v2.py
+++ osgconnect/gosync3/testing_globus_db_v2.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 import argparse
 import json
 from globus_db_v2 import globus_db
+from connect_db_v2 import connect_db_json as connect_db
 import collections
 import unicodedata
 
@@ -26,65 +27,66 @@ from util import parse_json_config
 
 
 
-def manage_user(globus_members, connect_users):
-    new_members = [gmem for gmem in globus_members
-                   if gmem["username"] not in connect_users.keys()]
-    gusernames = [gmem["username"] for gmem in globus_members]
-    deleted_members = [cmem for cmem in connect_users.keys()
-                       if cmem not in gusernames]
-    for gmem in globus_members:
-        gmem["name"] = unicodedata.normalize(
-            "NFKD", gmem["name"]).encode('ascii', 'ignore')
-    uids = [userinfo["uid"]
-            for username, userinfo in connect_users.iteritems()]
-    max_uid = max(uids)
-    for new_member in new_members:
-        if "@" in new_member["username"]:
-            print(new_member["username"])
-            continue
-        connect_users[new_member["username"]] = {
-            "auth_refresh_token": None,
-            "comment": new_member["name"],
-            "email": new_member["email"],
-            "gid": 1000,
-            "manage_group": False,
-            "nexus_refresh_token": None,
-            "shell": "/bin/bash",
-            "ssh_keys": decompose_sshkey(new_member),
-            "uid": max_uid + 1 if max_uid >= 100000 else 100000,
-            "groups": new_member["groups"]
-        }
-        max_uid = max_uid + 1 if max_uid >= 100000 else 100000
-        if len(new_member["groups"]) == 1:
-            print(new_member["username"])
-    for gmem in globus_members:
-        if gmem["username"] in new_members or "@" in gmem["username"]:
-            continue
-        json_ssh_key = decompose_sshkey(gmem)
-        connect_users[gmem["username"]]["ssh_keys"] = json_ssh_key
-        connect_users[gmem["username"]]["email"] = gmem["email"]
-        connect_users[gmem["username"]]["groups"] = gmem["groups"]
-    for dmem in deleted_members:
-        connect_users.pop(dmem, None)
-    return connect_users
-
+# def manage_user(globus_members, connect_users):
+#     new_members = [gmem for gmem in globus_members
+#                    if gmem["username"] not in connect_users.keys()]
+#     gusernames = [gmem["username"] for gmem in globus_members]
+#     deleted_members = [cmem for cmem in connect_users.keys()
+#                        if cmem not in gusernames]
+#     for gmem in globus_members:
+#         gmem["name"] = unicodedata.normalize(
+#             "NFKD", gmem["name"]).encode('ascii', 'ignore')
+#     uids = [userinfo["uid"]
+#             for username, userinfo in connect_users.iteritems()]
+#     max_uid = max(uids)
+#     for new_member in new_members:
+#         if "@" in new_member["username"]:
+#             print(new_member["username"])
+#             continue
+#         connect_users[new_member["username"]] = {
+#             "auth_refresh_token": None,
+#             "comment": new_member["name"],
+#             "email": new_member["email"],
+#             "gid": 1000,
+#             "manage_group": False,
+#             "nexus_refresh_token": None,
+#             "shell": "/bin/bash",
+#             "ssh_keys": decompose_sshkey(new_member),
+#             "uid": max_uid + 1 if max_uid >= 100000 else 100000,
+#             "groups": new_member["groups"]
+#         }
+#         max_uid = max_uid + 1 if max_uid >= 100000 else 100000
+#         if len(new_member["groups"]) == 1:
+#             print(new_member["username"])
+#     for gmem in globus_members:
+#         if gmem["username"] in new_members or "@" in gmem["username"]:
+#             continue
+#         json_ssh_key = decompose_sshkey(gmem)
+#         connect_users[gmem["username"]]["ssh_keys"] = json_ssh_key
+#         connect_users[gmem["username"]]["email"] = gmem["email"]
+#         connect_users[gmem["username"]]["groups"] = gmem["groups"]
+#     for dmem in deleted_members:
+#         connect_users.pop(dmem, None)
+#     return connect_users
 
 def main(args):
     config = parse_json_config(args.config)
-    globusdb = globus_db(config=config)
-    globus_groups = globusdb.get_all_groups()
-    globus_members = globusdb.get_all_users(get_user_groups=True)
-    # member_groups = globusdb.get_user_groups()
-    with open("test_groups.json", "r") as infile:
-        user_groups_db = json.load(infile)
-    user_groups_db["accounts::groups"] = find_alter_duplicate_gids(
-        user_groups_db["accounts::groups"])
-    user_groups_db["accounts::groups"] = append_unix_group(
-        globus_groups, user_groups_db["accounts::groups"])
-    user_groups_db["accounts::users"] = manage_user(
-        globus_members, user_groups_db["accounts::users"])
-    with open("test_groups_v2.json", "w") as outfile:
-        json.dump(user_groups_db, outfile)
+    connnectdb = connect_db(config=config)
+    globusdb = globus_db(config=config, connect_db=connnectdb)
+    globusdb.get_user_groups_profile("briedel")
+    # globus_groups = globusdb.get_all_groups()
+    # globus_members = globusdb.get_all_users(get_user_groups=True)
+    # # member_groups = globusdb.get_user_groups()
+    # with open("test_groups.json", "r") as infile:
+    #     user_groups_db = json.load(infile)
+    # user_groups_db["accounts::groups"] = find_alter_duplicate_gids(
+    #     user_groups_db["accounts::groups"])
+    # user_groups_db["accounts::groups"] = append_unix_group(
+    #     globus_groups, user_groups_db["accounts::groups"])
+    # user_groups_db["accounts::users"] = manage_user(
+    #     globus_members, user_groups_db["accounts::users"])
+    # with open("test_groups_v2.json", "w") as outfile:
+    #     json.dump(user_groups_db, outfile)
 
 
 if __name__ == '__main__':
